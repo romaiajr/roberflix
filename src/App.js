1b3780c15280ec857439dc185dfa15e61c08721d
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2020-07-28 13:32:17 -0300

    App pronto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
+import DI from "./data/dados_iniciais.json"
 import Menu from "./components/Menu"
 import BannerMain from "./components/BannerMain"
+import Carousel from "./components/Carousel"
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
+      <BannerMain
+        videoTitle="Rex Orange County - Sunflower [legendado]"
+        url="https://www.youtube.com/watch?v=NHZuvQ3Fz98"
+        videoDescription="Programando e ouvindo música"
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DI.categorias[0]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DI.categorias[1]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DI.categorias[2]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DI.categorias[3]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DI.categorias[4]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DI.categorias[5]}
+      />
+      <Footer />
     </div>
   )
 }
