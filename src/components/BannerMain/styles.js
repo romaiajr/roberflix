58410a4317242f9918f1b138772a6de595835bb2
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2020-07-29 19:29:56 -0300

    Button color

--- src/components/BannerMain/styles.js
+++ src/components/BannerMain/styles.js
@@ -113,9 +113,9 @@ export const WatchButton = styled.button`
   text-decoration: none;
   display: inline-block;
   border: 1px solid transparent;
-  color: var(--black);
-  background: var(--white);
-  border-color: var(--black);
+  color: var(--primary);
+  background-color: rgb(0,0,0,0.4);
+  border-color: var(--primary);
   transition: opacity .3s;
   display: none;
   margin: 0 auto;
