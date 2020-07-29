42f2d5767b71c12088acac16cb838c6a448354e8
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2020-07-29 20:06:11 -0300

    Footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/image/Logo.png'
 
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img src={Logo} alt="Logo Roberflix" className="Logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por <a href="https://github.com/romaiajr" style={{ color: "var(--primary)", textDecoration: "none" }}><b>Roberto Maia</b></a> durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
